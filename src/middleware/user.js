406b0224527a4936fa063af3419fa1d1365ca22e
Grant Schulte <schulte.grant@gmail.com>
2017-11-16 07:12:43 -0500

    Adds user profile model

--- src/middleware/user.js
+++ src/middleware/user.js
@@ -2,9 +2,7 @@ module.exports = () => {
   return (req, res, next) => {
     if (req.session.user) {
       res.locals.user = req.session.user;
-      console.log("RES.US", res.locals.user);
     }
-
     next();
   };
 };
